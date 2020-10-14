119ded883f541149deb026556bcf0b9bf8ad3f8e
masashi takeshita <masashibox@gmail.com>
2020-10-14 18:49:48 +0900

    flutterとの連携用functionsを追加

--- develop/functions/index.js
+++ develop/functions/index.js
@@ -3,6 +3,13 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+exports.helloWorld = functions.region('asia-northeast1').https.onRequest((request, response) => {
+    // response.send("Hello from Firebase!");
+    return {test: 'hello world'};
+   });
+
+exports.helloWorld2 = functions.region('asia-northeast1').https.onCall((data, context) => {
+    return {
+        test: 'hello world',
+      };
+  });
